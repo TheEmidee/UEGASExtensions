3e5bb840b1baf6e961436553bd2cfd14df0770fe
michael.delva <michael@emidee.net>
2022-08-01 15:05:35 +0200

    Added actor with abilities

---
+++ Source/GASExtensions/Private/GASExtActorWithAbilities.cpp
@@ -0,0 +1,41 @@
+#include "GASExtActorWithAbilities.h"
+
+#include "Abilities/GASExtAbilitySet.h"
+
+AGASExtActorWithAbilities::AGASExtActorWithAbilities()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    AbilitySystemComponent = CreateDefaultSubobject< UGASExtAbilitySystemComponent >( TEXT( "AbilitySystemComponent" ) );
+    AbilitySystemComponent->SetIsReplicated( true );
+}
+
+UAbilitySystemComponent * AGASExtActorWithAbilities::GetAbilitySystemComponent() const
+{
+    return AbilitySystemComponent;
+}
+
+void AGASExtActorWithAbilities::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+
+    check( AbilitySystemComponent != nullptr );
+    AbilitySystemComponent->InitAbilityActorInfo( this, this );
+}
+
+void AGASExtActorWithAbilities::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if ( !HasAuthority() )
+    {
+        return;
+    }
+
+    for ( const auto * ability_set : AbilitySets )
+    {
+        ability_set->GiveToAbilitySystem( AbilitySystemComponent, nullptr );
+    }
+
+    AbilitySystemComponent->SetTagRelationshipMapping( TagRelationshipMapping );
+}
