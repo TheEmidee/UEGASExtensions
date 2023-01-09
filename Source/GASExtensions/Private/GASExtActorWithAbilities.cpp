4873904c333ce713299ebdd961ea8847c1f94c45
Viktor Blomme <viktor.blomme@hotmail.com>
2023-01-09 16:41:57 +0100

    Expose attribute sets and create at runtime

--- Source/GASExtensions/Private/GASExtActorWithAbilities.cpp
+++ Source/GASExtensions/Private/GASExtActorWithAbilities.cpp
@@ -46,4 +46,11 @@ void AGASExtActorWithAbilities::BeginPlay()
         const auto spec_handle = AbilitySystemComponent->MakeOutgoingSpec( effect, 0, FGameplayEffectContextHandle() );
         AbilitySystemComponent->ApplyGameplayEffectSpecToSelf( *spec_handle.Data.Get() );
     }
+
+    for ( const auto attribute_set_class : AttributeSetClasses )
+    {
+        auto * attribute_set = NewObject< UAttributeSet >( this, attribute_set_class, NAME_None, RF_Transient );
+        AbilitySystemComponent->GetSpawnedAttributes_Mutable().Add( attribute_set );
+        AttributeSets.Add( attribute_set );
+    }
 }
